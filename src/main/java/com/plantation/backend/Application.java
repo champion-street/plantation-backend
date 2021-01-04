d044c28733659c7d83cec878fa6a8cbab97213e0
champion-street <champion19dev@gmail.com>
2021-01-04 22:43:42 +0100

    added manifest, changed @ComponentScan to appropriate package

--- src/main/java/com/plantation/backend/Application.java
+++ src/main/java/com/plantation/backend/Application.java
@@ -1,13 +1,13 @@
 package com.plantation.backend;
 
-import hu.locsoloapp.property.FileStorageProperties;
+import com.plantation.backend.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "hu.locsoloapp")
+@ComponentScan(basePackages = "com.plantation.backend")
 @EnableConfigurationProperties({FileStorageProperties.class})
 public class Application {
 
