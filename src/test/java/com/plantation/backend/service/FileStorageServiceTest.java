a61bcfae0aaffe563f8965fd8e8a38d5205a7593
adamszabolcs <szabolcs.adam91@gmail.com>
2021-09-17 16:51:53 +0200

    finished plantService tests, created jsonService test

---
+++ src/test/java/com/plantation/backend/service/FileStorageServiceTest.java
@@ -0,0 +1,77 @@
+package com.plantation.backend.service;
+
+import com.plantation.backend.Application;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class FileStorageServiceTest {
+
+    MultipartFile result;
+
+    @Autowired
+    FileStorageService fileStorageService;
+
+    @Before
+    public void getFile() {
+        Path path = Paths.get("E:\\_Munka\\plantation-backend\\src\\test\\resources\\testImages\\testimage.png");
+        String name = "testimage.png";
+        String originalFileName = "testimage.png";
+        String contentType = "image/png";
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        result = new MockMultipartFile(name, originalFileName, contentType, content);
+    }
+
+    @After
+    public void deleteFile() {
+        Path path = fileStorageService.getFileStorageLocation();
+        for (File file : path.toFile().listFiles()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void testStoreFileExtensionShouldBeTheSameAsOriginalFile() {
+        assertTrue(fileStorageService.storeFile(result).endsWith("png"));
+    }
+
+    @Test
+    public void testStoreFileDirectoryShouldContainOneFile() {
+        fileStorageService.storeFile(result);
+        assertEquals(1, new File("E:\\opt\\plantation-backend\\images").listFiles().length);
+    }
+
+    @Test
+    public void testDeleteFileShouldReturnTrue() {
+        fileStorageService.storeFile(result);
+        assertTrue(fileStorageService.deleteFile(new File("E:\\opt\\plantation-backend\\images").listFiles()[0].getName()));
+    }
+
+    @Test
+    public void testDeleteFileShouldReturnFalseWhenThereIsNoImage() {
+        assertFalse(fileStorageService.deleteFile("E:\\opt\\plantation-backend\\images\\asdasd.png"));
+    }
+}
--- src/test/java/com/plantation/backend/service/PlantServiceTest.java
+++ src/test/java/com/plantation/backend/service/PlantServiceTest.java
@@ -15,15 +15,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -47,15 +53,20 @@ public class PlantServiceTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    FileStorageService service;
+
     public Plant plant;
 
     public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
     public Object plantObject = new Object() {
-        public String name = "plantName";
-        public String description = "plantDescription";
-        public int wateringCycle = 2;
+        public final String name = "plantName";
+        public final String description = "plantDescription";
+        public final int wateringCycle = 2;
     };
+
+    public String bulkWaterIds = "{\"ids\":[\"1\", \"2\"]}";
 
     public String patchPlant = "[{\"op\":\"replace\",\"path\":\"/description\",\"value\":\"Szep\"}]";
 
@@ -71,21 +82,20 @@ public class PlantServiceTest {
     }
 
     @Test
-    public void getAllPlants() {
+    public void testGetAllPlantsShouldReturnOne() {
         List<Plant> plantList = plantService.getAllPlants();
         assertEquals(1, plantList.size());
     }
 
     @Test
-    public void getPlant() {
+    public void testGetPlantShouldReturnTheOnlyPlant() {
         List<Plant> plantList = plantRepository.findAll();
         long plantId = plantList.get(0).getId();
         assertEquals(plantList.get(0), plantService.getPlant(plantId));
     }
 
     @Test
-    public void applyPatchToPlant() throws IOException, JsonPatchException {
-        plant = plantRepository.findAll().get(0);
+    public void testApplyPatchToPlantShouldReturnTheUpdatedValue() throws IOException, JsonPatchException, ParseException {
         Date now = Calendar.getInstance().getTime();
         plant.setLastWateringDate(sdf.format(now));
         InputStream in = new ByteArrayInputStream(patchPlant.getBytes());
@@ -93,42 +103,51 @@ public class PlantServiceTest {
         assertEquals("Szep", plantService.applyPatchToPlant(patch, plant).getDescription());
     }
 
-    @Test
-    public void calculateAndSetWateringDeadline() {
+    @Test(expected = ParseException.class)
+    public void testCalculateAndSetWateringDeadlineShouldThrowParseException() throws ParseException {
+        plant.setLastWateringDate("asdasd");
+        plantService.calculateAndSetWateringDeadline(plant);
     }
 
     @Test
     @Transactional
-    public void addPlant() throws JsonProcessingException {
+    public void testAddPlantShouldReturn2() throws JsonProcessingException, ParseException {
         String plantJson = objectMapper.writeValueAsString(plantObject);
         Plant plant = plantService.addPlant(plantJson);
         assertEquals(2, plantRepository.findAll().size());
     }
 
     @Test
-    public void uploadPlantImage() {
+    public void testUploadPlantImageShouldReturnPNG() {
+        MultipartFile file = getFile();
+        assertTrue(plantService.uploadPlantImage(plant, file).endsWith("png"));
+        deleteFile();
     }
 
     @Test
-    public void updatePlantWatered() {
+    public void testUpdatePlantWateredShouldReturnTheSameDate() throws ParseException {
+        Date now = Calendar.getInstance().getTime();
+        Calendar c = Calendar.getInstance();
+        c.setTime(now);
+        c.add(Calendar.DAY_OF_MONTH, plant.getWateringCycleInDays());
+
+        assertEquals(sdf.format(c.getTime()), plantService.updatePlantWatered(plant).getWateringDeadline());
     }
 
     @Test
-    public void deletePlant() {
+    public void testDeletePlantShouldReturnEmptyDb() {
         plantService.deletePlant(plantRepository.findAll().get(0).getId());
         assertEquals(0, plantRepository.findAll().size());
     }
 
     @Test
-    public void deletePlantImage() {
+    public void testSetPlantImageURLShouldReturnTestImage() {
+        plantService.setPlantImageURL(plant, "testImage.png");
+        assertEquals("testImage.png", plantRepository.findAll().get(0).getImageURL());
     }
 
     @Test
-    public void setPlantImageURL() {
-    }
-
-    @Test
-    public void deleteAllPlants() {
+    public void testDeleteAllPlantsShouldReturnEmptyDb() {
         Plant newPlant = Plant.builder().name("name2").description("desc").wateringCycleInDays(1).build();
         plantRepository.save(newPlant);
         assertEquals(2, plantRepository.findAll().size());
@@ -138,5 +157,37 @@ public class PlantServiceTest {
 
     @Test
     public void bulkWaterPlants() {
+        Plant newPlant = Plant.builder().name("plantName").description("desc").wateringCycleInDays(2).lastWateringDate(sdf.format(Calendar.getInstance().getTime())).build();
+        plantRepository.save(newPlant);
+        assertEquals(2, plantRepository.findAll().size());
+        plantService.bulkWaterPlants(bulkWaterIds);
+        Date now = Calendar.getInstance().getTime();
+        Calendar c = Calendar.getInstance();
+        c.setTime(now);
+        c.add(Calendar.DAY_OF_MONTH, plant.getWateringCycleInDays());
+        assertEquals(sdf.format(c.getTime()), plantRepository.findAll().get(0).getWateringDeadline());
+        c.add(Calendar.DAY_OF_MONTH, -1);
+        assertEquals(sdf.format(c.getTime()), plantRepository.findAll().get(1).getWateringDeadline());
+    }
+
+    private MultipartFile getFile() {
+        Path path = Paths.get("E:\\_Munka\\plantation-backend\\src\\test\\resources\\testImages\\testimage.png");
+        String name = "testimage.png";
+        String originalFileName = "testimage.png";
+        String contentType = "image/png";
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new MockMultipartFile(name, originalFileName, contentType, content);
+    }
+
+    private void deleteFile() {
+        Path path = service.getFileStorageLocation();
+        for (File file : path.toFile().listFiles()) {
+            file.delete();
+        }
     }
 }
